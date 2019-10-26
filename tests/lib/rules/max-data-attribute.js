c5acb3760cddb6b669dfa27fd1cf527e7983593b
周浩磊 <1996yeyan@gmail.com>
2019-10-26 21:54:36 +0800

    fix: 修复 data 为函数表达式时规则无法判断的情况

--- tests/lib/rules/max-data-attribute.js
+++ tests/lib/rules/max-data-attribute.js
@@ -74,6 +74,29 @@ tester.run('max-data-attribute', rule, {
       filename: 'test.vue',
       code: `
           export default {
+            data:function() {
+              return {
+                msg: 1,
+                msg2: 1,
+                msg3: 1,
+                msg4: 1,
+                msg5: 1,
+                msg6: 1,
+                msg7: 1,
+                msg8: 1,
+                msg9: 1
+              }
+            },
+          }
+      `,
+      errors: [{
+        message: 'data 根属性的数量最大为 8 个',
+      }],
+    },
+    {
+      filename: 'test.vue',
+      code: `
+          export default {
             data:()=> {
               return {
                 msg: 1,
@@ -91,6 +114,32 @@ tester.run('max-data-attribute', rule, {
       `,
       errors: [{
         message: 'data 根属性的数量最大为 8 个',
+      }],
+    },
+
+    {
+      filename: 'test.vue',
+      options: [5],
+      code: `
+        export default {
+          data () {
+          let a = 1
+            return {
+              msg: 1,
+              msg2: 1,
+              msg3: 1,
+              msg4: 1,
+              msg5: 1,
+              msg6: 1,
+              msg7: 1,
+              msg8: 1,
+              msg9: 1
+            }
+          },
+        }
+      `,
+      errors: [{
+        message: 'data 根属性的数量最大为 5 个',
       }],
     },
     {
